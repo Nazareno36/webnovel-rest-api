5e34645d4caa8e175a4d6cb42ce15b7ab53127fe
carlosa paez <carlosa.paezc@autonoma.edu.co>
2023-08-13 17:13:01 -0500

    testing setup added

--- src/controllers/novels.controller.js
+++ src/controllers/novels.controller.js
@@ -7,7 +7,7 @@ const novelsCtrl = {
 			res.json(novels);
 		} catch (error) {
 			console.error(error);
-			res.status(400).send(error.message);
+			res.status(400).json(error.message);
 		}
 	},
 
@@ -26,12 +26,37 @@ const novelsCtrl = {
 		try {
 			const newNovel = new Novel(req.body);
 			await newNovel.save();
-			res.json({ id: newNovel.id });
+			res.status(201).json({ id: newNovel.id });
 		} catch (error) {
 			console.error(error);
 			res.status(400).send(error.message);
 		}
 	},
+
+	deleteNovels: async (req, res) => {
+		try {
+			const deletedNovels = await Novel.deleteMany();
+			console.log(`Deleted novels: ${deletedNovels}`);
+			if (deletedNovels != null) res.sendStatus(204);
+			else res.sendStatus(404);
+		} catch (error) {
+			console.error(error);
+			res.status(400).send(error.message);
+		}
+	},
+
+	deleteNovelById: async (req, res) => {
+		try {
+			const deletedNovel = await Novel.findByIdAndDelete(req.params.novelId);
+			console.log(`Deleted novel: ${deletedNovel}`);
+			if (deletedNovel != null) res.sendStatus(204);
+			else res.sendStatus(404);
+		} catch (error) {
+			console.error(error);
+			res.status(400).send(error.message);
+		}
+	}
+
 };
 
 module.exports = novelsCtrl;
--- src/routers/v1/novels/index.js
+++ src/routers/v1/novels/index.js
@@ -4,7 +4,10 @@ const novelsCtrl = require("../../../controllers/novels.controller");
 novelsRouter.get("/", novelsCtrl.getNovels);
 novelsRouter.get("/:novelId", novelsCtrl.getNovelById);
 
-novelsRouter.post("J");
+novelsRouter.post("/", novelsCtrl.createNovel);
+
+novelsRouter.delete("/", novelsCtrl.deleteNovels);
+novelsRouter.delete("/:novelId", novelsCtrl.deleteNovelById);
 
 novelsRouter.use("/:novelId/books", require("../books"));
 
