45c1a7740919a4b74258430128d2c588b27ad8dc
carlosa paez <carlosa.paezc@autonoma.edu.co>
2023-08-14 00:24:33 -0500

    some test

--- src/tests/novels.test.js
+++ src/tests/novels.test.js
@@ -4,15 +4,12 @@ const { describe, it } = require("mocha");
 const expect = require("chai").expect;
 
 describe("POST /novels", () => {
-	
 	it("should return a 201 status", async () => {
-		const response = await request
-			.post("/novels")
-			.send({
-				title: "Test",
-				synopsis: "Test",
-				author: "Test",
-			});
+		const response = await request.post("/novels").send({
+			title: "Test",
+			synopsis: "Test",
+			author: "Test",
+		});
 		expect(response.status).eql(201);
 		expect(response.body.id).exist;
 
@@ -21,29 +18,24 @@ describe("POST /novels", () => {
 });
 
 describe("GET /novels", () => {
-
 	it("should return a 200 status", async () => {
 		const response = await request.get("/novels");
 		expect(response.status).eql(200);
 	});
 
 	it("should return a 200 status", async () => {
-
 		const testBody = {
 			title: "Test",
 			synopsis: "Test",
 			author: "Test",
-		} 
+		};
 
-		const postRes = await request
-			.post("/novels")
-			.send(testBody);
+		const postRes = await request.post("/novels").send(testBody);
 
 		const response = await request.get(`/novels/${postRes.body.id}`);
 		expect(response.status).eql(200);
 		expect(response.body).contain(testBody);
 
 		const delRes = await request.delete(`/novels/${postRes.body.id}`);
-
 	});
 });
